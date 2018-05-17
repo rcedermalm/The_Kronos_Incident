9802e44473e611fde95b80835aabe42517aea03d
Rebecca Cedermalm <rebce973@student.liu.se>
2018-05-17 10:59:46 +0200

    Small changes to make merge possible

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ d3.queue()
 	.defer(d3.csv, "data/location.csv")
     .await(analyze);
 
+// Kolla upp om folk är på samma ställe. 
+// Har någon lånat ut sitt loyalty card till någon? (De kan vara på samma ställe)
+
 function analyze(error, gps, ccData, carAssign, loyalty, locationData){
     if (error) {
         console.log(error);
@@ -13,6 +16,7 @@ function analyze(error, gps, ccData, carAssign, loyalty, locationData){
 
     var persons = get_persons(gps, carAssign, ccData, locationData);
     find_general_person(persons);
+    //check_truckers(gps, persons, locationData)
 
     getMap(gps, locationData, ccData, carAssign);
 }
