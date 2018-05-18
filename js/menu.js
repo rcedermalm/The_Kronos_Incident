b1dc418db9bba1b391395fc92034eac6961aac58
Johan Eriksson <johan@minasidor.com>
2018-05-18 13:28:37 +0200

    add start and end time in gui

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,9 @@
 function menu(){
   var div = '#id-select';
+  var div2 = '#starttime';
+  var div3 = '#endtime';
+  var div4 = '#update';
+
   var list = []
   var truck = [101,104,105,106,107];
   for (var i = 1; i < 36; i++) {
@@ -16,6 +20,44 @@ function menu(){
       .attr('id','selectID' )
       .on('change',onchange);
 
+      var startTime = d3.select(div2).text("start: ")
+      .append('input')
+    .attr('type','text')
+    .attr('name','textInput')
+    .attr('id','startInput')
+    .attr('value','01/06/2014 00:00:01');
+
+    var startTime = d3.select(div3).text("End: ")
+    .append('input')
+  .attr('type','text')
+  .attr('name','textInput')
+  .attr('id','endInput')
+  .attr('value','01/19/2014 23:59:59');
+
+
+  var button = d3.select(div4)
+  .append("input")
+  .attr("type", "button")
+  .attr('name', 'update')
+  .attr('text', update)
+  .attr('value','Update')
+  .on('click',onclick);
+
+
+  function onclick(){
+    var startD = document.getElementById("startInput").value;
+    var endD = document.getElementById("endInput").value;
+    var format = d3.utcParse('%m/%d/%Y %H:%M:%S');
+    selected = document.getElementById("selectID").value;
+
+      if (!isNaN(format(startD)) && !isNaN(format(endD)) && format(startD) < format(endD)) {
+        map.show(selected, startD, endD);
+    }
+
+
+  }
+
+
       var options = select
         .selectAll('option')
       	.data(list)
@@ -26,7 +68,13 @@ function menu(){
       function onchange() {
       	selected = document.getElementById("selectID").value;
         //console.log(selected);
-        map.show(selected);
+        var startD = document.getElementById("startInput").value;
+        var endD = document.getElementById("endInput").value;
+        var format = d3.utcParse('%m/%d/%Y %H:%M:%S');
+        if (!isNaN(format(startD)) && !isNaN(format(endD)) && format(startD) < format(endD)) {
+          map.show(selected, startD, endD);
+        }
+
         //sp.selectDots(mlist.getValue());
       };
 
